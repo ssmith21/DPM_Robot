32acaf80d2618430487f299cd852287821fcf83e
ssmith21 <smithsean514@gmail.com>
2021-04-04 15:24:24 -0400

    changed motor speed wow

--- controllers/T03Controller/ca/mcgill/ecse211/project/Resources.java
+++ controllers/T03Controller/ca/mcgill/ecse211/project/Resources.java
@@ -101,10 +101,10 @@ public class Resources {
   public static final double BASE_WIDTH = 0.158;
   
   /** The speed at which the robot moves forward in degrees per second. */
-  public static final int FORWARD_SPEED = 300;
+  public static final int FORWARD_SPEED = 500;
   
   /** The speed at which the robot rotates in degrees per second. */
-  public static final int ROTATE_SPEED = 100;
+  public static final int ROTATE_SPEED = 300;
   
   /** The motor acceleration in degrees per second squared. */
   public static final int ACCELERATION = 1000;
