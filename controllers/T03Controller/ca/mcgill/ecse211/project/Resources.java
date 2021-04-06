05eae41049d01a68b6a6e9569e4d9db6252a65cd
Narry <nariman.zendehrooh@gmail.com>
2021-04-05 23:49:31 -0400

    Adjusting speed

--- controllers/T03Controller/ca/mcgill/ecse211/project/Resources.java
+++ controllers/T03Controller/ca/mcgill/ecse211/project/Resources.java
@@ -94,7 +94,7 @@ public class Resources {
   public static final double BASE_WIDTH = 0.158;
   
   /** The speed at which the robot moves forward in degrees per second. */
-  public static final int FORWARD_SPEED = 250;
+  public static final int FORWARD_SPEED = 350;
   
   /** The speed at which the robot rotates in degrees per second. */
   public static final int ROTATE_SPEED = 350;
